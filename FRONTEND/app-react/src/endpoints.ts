9452720ccc9b079e539999a64ca50d562114e8df
Denilson-CM <denilsoncolli37@gmail.com>
2023-02-04 15:36:25 -0600

    endponits

--- FRONTEND/app-react/src/endpoints.ts
+++ FRONTEND/app-react/src/endpoints.ts
@@ -3,7 +3,7 @@ const baseURL = process.env.REACT_APP_API_URL;
 //CATEGORIAS
 export const urlListaC = `${baseURL}/Categoria/Lista`;
 export const urlGuardarC = `${baseURL}/Categoria/Guardar`;
-export const urlEliminarC = `${baseURL}/Categoria/Eliminar/$`;
+export const urlEliminarC = `${baseURL}/Categoria/Eliminar`;
 
 //PRODUCTOS
 export const urlListaP = `${baseURL}/Producto/Lista`;
