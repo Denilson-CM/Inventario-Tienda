71c557d20a8bd3e6b7c3205cb9fc7787e944a097
AlexisKcc8 <CodeKJS863@gmail.com>
2023-02-08 13:34:27 -0600

    lista de catgorias pintadas

--- FRONTEND/app-react/src/pages/NewCategory/NewCategoryPage.tsx
+++ FRONTEND/app-react/src/pages/NewCategory/NewCategoryPage.tsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react";
 import { FormProductCategory } from "../../components/FormProductCategory/FormProductCategory";
 import { TableCategory } from "../../components/TableCategory/TableCategory";
+import { urlListaC } from "../../endpoints";
 import { useFormCategory } from "../../hooks/useFormCategory";
+
+import { Categories } from "../../interfaces/types";
 export const NewCategoryPage = () => {
   const { categories } = useFormCategory();
+
+  const [listApiCategories, setListApiCategories] = useState<Array<Categories>>(
+    []
+  );
+  const getCategories = async () => {
+    let resApi = await fetch(urlListaC);
+    let jsonListCategories = await resApi.json();
+    setListApiCategories(jsonListCategories);
+  };
+  useEffect(() => {
+    getCategories();
+  }, []);
 
   return (
     <section className="container bg-danger">
       <article className="row">
         <section className="col-8">
-          <TableCategory />
+          <TableCategory listApiCategories={listApiCategories} />
         </section>
         <section className="col-4">
           <FormProductCategory />
