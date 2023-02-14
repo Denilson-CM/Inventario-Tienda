d4c220cc2a1477320834738e259f3519ba73c0a0
AlexisKcc8 <CodeKJS863@gmail.com>
2023-02-14 11:14:57 -0600

    helpHttp añadido
    loader añadido

--- FRONTEND/app-react/src/pages/NewCategory/NewCategoryPage.tsx
+++ FRONTEND/app-react/src/pages/NewCategory/NewCategoryPage.tsx
@@ -1,27 +1,12 @@
-import { useEffect, useState } from "react";
 import { BtnShowModal } from "../../components/BtnShowModal/BtnShowModal";
 import { FormProductCategory } from "../../components/FormProductCategory/FormProductCategory";
+import { Loader } from "../../components/Loader/Loader";
 import { ModalInfo } from "../../components/ModalInfo/ModalInfo";
 import { TableCategory } from "../../components/TableCategory/TableCategory";
-import { urlListaC } from "../../endpoints";
-import { useFormCategory } from "../../hooks/useFormCategory";
+import { useCategories } from "../../hooks/useCategories";
 
-import { Categories } from "../../interfaces/types";
 export const NewCategoryPage = () => {
-  const { categories } = useFormCategory();
-
-  const [listApiCategories, setListApiCategories] = useState<Array<Categories>>(
-    []
-  );
-  const getCategories = async () => {
-    let resApi = await fetch(urlListaC);
-    let jsonListCategories = await resApi.json();
-    setListApiCategories(jsonListCategories);
-  };
-  useEffect(() => {
-    getCategories();
-  }, []);
-
+  const { listApiCategories, loading, error } = useCategories();
   return (
     <section className="container">
       <section className="row mb-3 ">
@@ -40,13 +25,61 @@ export const NewCategoryPage = () => {
       </section>
       <article className="row ">
         <section className="col-12 d-flex justify-content-center align-items-center border ">
-          <TableCategory listApiCategories={listApiCategories} />
+          {loading ? (
+            <Loader />
+          ) : (
+            <TableCategory>
+              <thead>
+                <tr>
+                  <th scope="col">Categoria</th>
+                  <th scope="col">Descripción</th>
+                  <th scope="col">Fecha de registro</th>
+                </tr>
+              </thead>
+              <tbody>
+                {!error ? (
+                  listApiCategories.map((categorie) => (
+                    <tr key={categorie?.id}>
+                      {/* <th scope="row">{categorie?.id}</th> */}
+                      <td>{categorie?.nombre}</td>
+                      <td>{categorie?.descripcion}</td>
+                      <td>{categorie?.createdAt.slice(0, 10)}</td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan={5} className="bg-danger text-center">
+                      <h2>Sin datos</h2>
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </TableCategory>
+          )}
         </section>
       </article>
       <ModalInfo titleModal="Agregar Categorias">
         <section>
           <FormProductCategory />
-          <TableCategory listApiCategories={[]} />
+          <TableCategory>
+            <thead>
+              <tr>
+                <th scope="col">Categoria</th>
+                <th scope="col">Descripción</th>
+                <th scope="col">Fecha de registro</th>
+              </tr>
+            </thead>
+            <tbody>
+              {listApiCategories.map((categorie) => (
+                <tr key={categorie?.id}>
+                  {/* <th scope="row">{categorie?.id}</th> */}
+                  <td>{categorie?.nombre}</td>
+                  <td>{categorie?.descripcion}</td>
+                  <td>{categorie?.createdAt.slice(0, 10)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </TableCategory>
         </section>
       </ModalInfo>
     </section>
